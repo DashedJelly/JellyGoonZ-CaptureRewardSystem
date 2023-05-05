ae4e0c1d08ab91a57a9e648969ba9a252f3add98
Your Name <jelly@thejellycollective.club>
2023-05-04 23:23:07 -0700

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,47 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
+import { ConnectWallet, MediaRenderer } from "@thirdweb-dev/react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
   return (
+    
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+      <ConnectWallet/>
+      {/* Top Section */}
+      <h1 className={styles.h1}>LAB 33 : G00NZ</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/mint`)}
+        >
+          {/* Mint a new NFT */}
+          <img src="https://ipfs.thirdwebcdn.com/ipfs/QmY5ZLAHfGTa27pEpS7ZCMnV625wveTBfAkboiPNENCAaU/goonzgif.gif" alt="drop" width={300} height={300} />
+          <h2 className={styles.selectBoxTitle}>JELLYG00NZ MINT</h2>
+          <p className={styles.selectBoxDescription}>
+            Mint a JELLYG00N for 3333 $JELLY
+          </p>
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <MediaRenderer src="ipfs://QmcPsp5aqMMCNdqk4igfPPuo2KqjoW3AFaR5hGww8iPd5R/mush%20lab.png" />
+          <h2 className={styles.selectBoxTitle}>Capture Reward System</h2>
+          <p className={styles.selectBoxDescription}>
+            Bring All Captured JellyGoonz into the Lab Please...
+          </p>
         </div>
-      </main>
-    </div>
+        </div>
+      </div>
+    
   );
 };
 
---
+++ pages/mint.tsx
@@ -0,0 +1,37 @@
+import { Web3Button } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { nftDropContractAddress } from "../consts/contractAddresses";
+import styles from "../styles/Home.module.css";
+
+const Mint: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>JELLYG00NZ</h1>
+
+      <p className={styles.explain}>
+        MINT YOUR JELLYGOON HERE
+      </p>
+      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
+
+      <Web3Button
+        theme="dark"
+        contractAddress={nftDropContractAddress}
+        action={(contract) => contract.erc721.claim(1)}
+        onSuccess={() => {
+          alert("NFT Claimed!");
+          router.push("/stake");
+        }}
+        onError={(error) => {
+          alert(error);
+        }}
+      >
+        Mint for 3333$JELLY
+      </Web3Button>
+    </div>
+  );
+};
+
+export default Mint;
